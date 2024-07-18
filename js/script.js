e8fea6473017075a52e648ab402442eee34980d4
nmst0811 <148926959+nmst0811@users.noreply.github.com>
2024-07-18 16:22:30 +0900

    change0718

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   // スタートボタンの要素を取得
   const startButton = document.getElementById('startButton');
+  // 設定ボタンの要素を取得
+  const settingButton = document.getElementById('settingButton');
+  // タイトルに戻るボタンの要素を取得
+  const back_titleSceneButton = document.getElementById('back_titleSceneButton');
   // タイトル画面の要素を取得
   const titleScene = document.getElementById('titleScene');
+  // 設定画面の要素を取得
+  const settingScene = document.getElementById('settingScene');
   // ゲームプレイ画面の要素を取得
   const gameScene = document.getElementById('gameScene');
 
@@ -13,6 +19,22 @@ startButton.addEventListener('click', () => {
   titleScene.style.display = 'none';
   // ゲームプレイ画面を表示する
   gameScene.style.display = 'block';
+});
+
+// 設定ボタンがクリックされたときの処理
+settingButton.addEventListener('click', () => {
+  // タイトル画面を非表示にする
+  titleScene.style.display = 'none';
+  // ゲームプレイ画面を表示する
+  settingScene.style.display = 'block';
+});
+
+// タイトルに戻るボタンがクリックされたときの処理
+back_titleSceneButton.addEventListener('click', () => {
+  // タイトル画面を非表示にする
+  settingScene.style.display = 'none';
+  // ゲームプレイ画面を表示する
+  titleScene.style.display = 'block';
 });
 
 const stage = document.getElementById("stage");
