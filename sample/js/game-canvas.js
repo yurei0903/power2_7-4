29def6356fb1424cc15afef5266bea15baa5220a
nmst0811 <148926959+nmst0811@users.noreply.github.com>
2024-10-24 17:07:01 +0900

    10/24

--- sample/js/game-canvas.js
+++ sample/js/game-canvas.js
@@ -4,8 +4,8 @@ const gameCanvas = {};
 // キャンバスの生成
 gameCanvas.genCanvas = function(w, h) {
     const canvas = document.createElement('canvas');    // 生成
-    canvas.width = w;   // 横幅設定
-    canvas.height = h;  // 高さ設定
+    canvas.width = 1920;   // 横幅設定
+    canvas.height = 1080;  // 高さ設定
     const context = canvas.getContext('2d');    // 2Dコンテキスト
     return {canvas, context, w, h};
 };
