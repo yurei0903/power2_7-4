e1cf85ae1b4af708657a65667a395a87a98d191a
nmst0811 <148926959+nmst0811@users.noreply.github.com>
2024-10-24 16:40:19 +0900

    yes 39 music

--- sample/js/app-layout.js
+++ sample/js/app-layout.js
@@ -4,15 +4,15 @@ const appLayout = {};
 // レイアウトの初期化
 appLayout.init = function() {
     this.fitSize = Math.min(window.innerWidth, window.innerHeight);
-    this.unit = Math.trunc(this.fitSize * 0.1);
+    this.unit = Math.trunc(this.fitSize * 0.127);
 
     const u = this.unit;
-    this.rectBoard = {x: u, y: u * 1.5, w: u * 8, h: u * 8};
+    this.rectBoard = {x: u, y: u * 1.4, w: u * 8, h: u * 8};
     this.scoreTexts = [
-        {x: u * 3.4, y: u * 0.75, w: u * 2.8},
-        {x: u * 7.4, y: u * 0.75, w: u * 2.8}
+        {x: u * 2.8, y: u * 0.85, w: u * 2.8},
+        {x: u * 6.0, y: u * 0.85, w: u * 2.8}
     ];
-    this.scoreImages = [{x: u, y: u * 0.25}, {x: u * 5, y: u * 0.25}];
+    this.scoreImages = [{x: u * 0.7, y: u * 0.3}, {x: u * 3.9, y: u * 0.3}];
 };
 
 // マスXYを画面XYに変換
